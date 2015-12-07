7e6b231dacbd876ac6de6fccce60ad4d8c0ac710
Inndy <inndy.tw@gmail.com>
2015-12-07 20:25:38 +0800

    Dynamic generate posts

--- guestbook.py
+++ guestbook.py
@@ -1,8 +1,21 @@
-from bottle import run, route, template, static_file
+from bottle import run, route, template, static_file, view
 
 @route('/')
+@view('static/index.html')
 def index():
-	return template('static/index.html')
+	posts = [
+		{
+			'author': 'Inndy',
+			'message': 'Hello, Bottle.py'
+		}
+	]
+
+	for i in range(10):
+		posts.append({
+			'author': 'User #%d' % (i + 100),
+			'message': 'I Love Python. ' * (i + 1)
+		})
+	return { 'posts': posts }
 
 @route('/static/<filename:path>')
 def static(filename):
