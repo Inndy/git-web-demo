fb83f60513b2ce7478b558068d962db9df12ff42
Inndy <inndy.mail@gmail.com>
2015-12-21 19:58:43 +0800

    Read data from data.json

--- guestbook.py
+++ guestbook.py
@@ -4,18 +4,11 @@ from bottle import run, route, template, static_file, view, request, redirect
 @route('/')
 @view('static/index.html')
 def index():
-	posts = [
-		{
-			'author': 'Inndy',
-			'message': 'Hello, Bottle.py'
-		}
-	]
-
-	for i in range(10):
-		posts.append({
-			'author': 'User #%d' % (i + 100),
-			'message': 'I Love Python. ' * (i + 1)
-		})
+	try:
+		with open('data.json', 'r') as fin:
+			posts = json.load(fin)
+	except FileNotFoundError as e:
+		posts = []
 	return { 'posts': posts }
 
 @route('/static/<filename:path>')
